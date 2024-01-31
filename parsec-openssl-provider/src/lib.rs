e222a17410f9964a14dc81da72703b4f75c434a7
Gowtham Suresh Kumar <gowtham.sureshkumar@arm.com>
2024-01-31 15:38:54 +0000

    openssl-provider: Add parsec NULL provider library
    This patch adds the NULL provider functionality without any crypto
    support. The idea of this patch is to provide a working openssl provider
    to add features on top off.
    
    Signed-off-by: Gowtham Suresh Kumar <gowtham.sureshkumar@arm.com>

---
+++ parsec-openssl-provider/src/lib.rs
@@ -0,0 +1,77 @@
+// Copyright 2023 Contributors to the Parsec project.
+// SPDX-License-Identifier: Apache-2.0
+use std::mem;
+
+pub use openssl_errors;
+pub use parsec_openssl2;
+
+use parsec_openssl2::{openssl_binding, types};
+
+use openssl_binding::{
+    OSSL_CORE_HANDLE, OSSL_DISPATCH, OSSL_FUNC_PROVIDER_GETTABLE_PARAMS,
+    OSSL_FUNC_PROVIDER_GET_PARAMS, OSSL_FUNC_PROVIDER_QUERY_OPERATION,
+};
+
+mod provider;
+use provider::*;
+
+mod catch;
+use catch::r#catch;
+
+// The init function populates the dispatch table and returns a NULL pointer
+// to the provider context. This needs to be changed when key management and
+// crypto support is added to the provider.
+pub unsafe fn parsec_provider_provider_init(
+    _handle: *const OSSL_CORE_HANDLE,
+    _in_: *const OSSL_DISPATCH,
+    out: *mut *const OSSL_DISPATCH,
+    provctx: types::VOID_PTR_PTR,
+) -> Result<(), parsec_openssl2::Error> {
+    let parsec_provider_gettable_params_ptr: ProviderGettableParamsPtr =
+        parsec_provider_gettable_params;
+
+    let parsec_provider_get_params_ptr: ProviderGetParamsPtr = parsec_provider_get_params;
+
+    let parsec_provider_query_ptr: ProviderQueryPtr = parsec_provider_query;
+
+    static mut DISPATCH_TABLE: [OSSL_DISPATCH; 4] = [parsec_openssl2::ossl_dispatch!(); 4];
+    static RESULT_INIT: std::sync::Once = std::sync::Once::new();
+
+    RESULT_INIT.call_once(|| {
+        DISPATCH_TABLE = [
+            parsec_openssl2::ossl_dispatch!(
+                OSSL_FUNC_PROVIDER_GETTABLE_PARAMS,
+                parsec_provider_gettable_params_ptr
+            ),
+            parsec_openssl2::ossl_dispatch!(
+                OSSL_FUNC_PROVIDER_GET_PARAMS,
+                parsec_provider_get_params_ptr
+            ),
+            parsec_openssl2::ossl_dispatch!(
+                OSSL_FUNC_PROVIDER_QUERY_OPERATION,
+                parsec_provider_query_ptr
+            ),
+            parsec_openssl2::ossl_dispatch!(),
+        ];
+    });
+
+    *out = DISPATCH_TABLE.as_ptr();
+    *provctx = std::ptr::null_mut();
+
+    Ok(())
+}
+
+openssl_errors::openssl_errors! {
+    #[allow(clippy::empty_enum)]
+    library Error("parsec_openssl_provider") {
+        functions {
+            PROVIDER_GETTABLE_PARAMS("parsec_provider_gettable_params");
+            PROVIDER_GET_PARAMS("parsec_provider_get_params");
+            PROVIDER_QUERY("parsec_provider_query");
+        }
+
+        reasons {
+            MESSAGE("");
+        }
+    }
+}
---
+++ parsec-openssl-provider/src/provider.rs
@@ -0,0 +1,90 @@
+// Copyright 2023 Contributors to the Parsec project.
+// SPDX-License-Identifier: Apache-2.0
+
+use parsec_openssl2::openssl_provider_param::{
+    OSSL_PROV_PARAM_BUILDINFO, OSSL_PROV_PARAM_NAME, OSSL_PROV_PARAM_STATUS,
+    OSSL_PROV_PARAM_VERSION,
+};
+
+use parsec_openssl2::{
+    locate_and_set_provider_status_param, locate_and_set_utf8_param, ossl_param, OPENSSL_ERROR,
+    OPENSSL_SUCCESS, OSSL_PROVIDER,
+};
+
+use crate::openssl_binding::{
+    OSSL_ALGORITHM, OSSL_PARAM, OSSL_PARAM_INTEGER, OSSL_PARAM_UTF8_PTR,
+};
+// Parsec provider parameters
+pub const PARSEC_PROVIDER_NAME: &[u8; 24] = b"Parsec OpenSSL Provider\0";
+pub const PARSEC_PROVIDER_VERSION: &[u8; 6] = b"0.1.0\0";
+
+// The types of parameters the provider supplies to the openssl library
+const PARSEC_PROVIDER_PARAM_TYPES: [OSSL_PARAM; 5] = [
+    // Provider name
+    ossl_param!(OSSL_PROV_PARAM_NAME, OSSL_PARAM_UTF8_PTR),
+    // Provider version
+    ossl_param!(OSSL_PROV_PARAM_VERSION, OSSL_PARAM_UTF8_PTR),
+    // Build info
+    ossl_param!(OSSL_PROV_PARAM_BUILDINFO, OSSL_PARAM_UTF8_PTR),
+    // Provider Status
+    ossl_param!(OSSL_PROV_PARAM_STATUS, OSSL_PARAM_INTEGER),
+    ossl_param!(),
+];
+
+// Returns an array of OpenSSL parameter types that the
+// provider supports
+pub unsafe extern "C" fn parsec_provider_gettable_params(
+    _provider: *const OSSL_PROVIDER,
+) -> *const OSSL_PARAM {
+    PARSEC_PROVIDER_PARAM_TYPES.as_ptr()
+}
+
+// Populates the provider's name, version and the status to parameter structure
+pub unsafe extern "C" fn parsec_provider_get_params(
+    _provctx: *const OSSL_PROVIDER,
+    params: *mut OSSL_PARAM,
+) -> ::std::os::raw::c_int {
+    let result = super::r#catch(Some(|| super::Error::PROVIDER_GET_PARAMS), || {
+        // Find parameter of type OSSL_PROV_PARAM_NAME and populate it with PARSEC_PROVIDER_NAME
+        locate_and_set_utf8_param(OSSL_PROV_PARAM_NAME, PARSEC_PROVIDER_NAME, params)?;
+
+        // Find parameter of type OSSL_PROV_PARAM_VERSION and populate it with PARSEC_PROVIDER_VERSION
+        locate_and_set_utf8_param(OSSL_PROV_PARAM_VERSION, PARSEC_PROVIDER_VERSION, params)?;
+
+        // Find parameter of type OSSL_PROV_PARAM_STATUS and populate it with status 1
+        locate_and_set_provider_status_param(params)?;
+
+        Ok(OPENSSL_SUCCESS)
+    });
+    match result {
+        Ok(result) => result,
+        Err(()) => OPENSSL_ERROR,
+    }
+}
+
+// Function pointer of type OSSL_FUNC_PROVIDER_GETTABLE_PARAMS
+pub type ProviderGettableParamsPtr =
+    unsafe extern "C" fn(*const OSSL_PROVIDER) -> *const OSSL_PARAM;
+
+// Function pointer of type OSSL_FUNC_PROVIDER_GET_PARAMS
+pub type ProviderGetParamsPtr = unsafe extern "C" fn(
+    provctx: *const OSSL_PROVIDER,
+    params: *mut OSSL_PARAM,
+) -> ::std::os::raw::c_int;
+
+// Function pointer of type OSSL_FUNC_PROVIDER_QUERY_OPERATION
+pub type ProviderQueryPtr = unsafe extern "C" fn(
+    prov: *mut OSSL_PROVIDER,
+    operation_id: ::std::os::raw::c_int,
+    no_cache: *mut ::std::os::raw::c_int,
+) -> *const OSSL_ALGORITHM;
+
+// The null provider implementation currently doesn't supply any algorithms to the core
+pub unsafe extern "C" fn parsec_provider_query(
+    _prov: *mut OSSL_PROVIDER,
+    _operation_id: ::std::os::raw::c_int,
+    no_cache: *mut ::std::os::raw::c_int,
+) -> *const OSSL_ALGORITHM {
+    *no_cache = 0;
+    std::ptr::null_mut()
+}
