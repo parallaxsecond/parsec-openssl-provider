46afd6772f1e4f695917d97e28b34068ca96655f
Gowtham Suresh Kumar <gowtham.sureshkumar@arm.com>
2024-01-31 15:36:50 +0000

    openssl2: Add openssl2 support functions
    This patch adds macros and other functions required for setting up
    the provider.
    
    Signed-off-by: Gowtham Suresh Kumar <gowtham.sureshkumar@arm.com>

---
+++ parsec-openssl2/src/types.rs
@@ -0,0 +1,5 @@
+// Copyright 2024 Contributors to the Parsec project.
+// SPDX-License-Identifier: Apache-2.0
+#![allow(non_camel_case_types)]
+pub type VOID_PTR = *mut std::os::raw::c_void;
+pub type VOID_PTR_PTR = *mut VOID_PTR;
