ec17350b7120751b51e4b245070cb2b1461bbda1
Gowtham Suresh Kumar <gowtham.sureshkumar@arm.com>
2024-02-19 15:41:59 +0000

    ci.sh: Build and test provider
    The CI script is now updated with build steps and basic test commands
    to verify the dynamic loading of the parsec provider.
    
    Signed-off-by: Gowtham Suresh Kumar <gowtham.sureshkumar@arm.com>

--- ci.sh
+++ ci.sh
@@ -7,3 +7,26 @@ set -ex
 
 echo "OpenSSL version being used:"
 openssl version
+
+# Build parsec provider shared library
+pushd parsec-openssl-provider-shared/ &&
+cargo build
+popd
+
+# Try loading the build parsec provider 
+provider_load_result=$(openssl list -providers -provider-path ./target/debug/ -provider libparsec_openssl_provider_shared)
+echo $provider_load_result
+
+test_string='Providers:
+  libparsec_openssl_provider_shared
+    name: Parsec OpenSSL Provider
+    version: 0.1.0
+    status: active'
+
+if [[ $test_string == $provider_load_result ]]; then
+    echo "Parsec OpenSSL Provider loaded successfully!!!!"
+    exit 0;
+fi
+
+echo "Loaded Provider has unexpected parameters!!!!"
+exit 1
