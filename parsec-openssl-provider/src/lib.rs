e19c566511c67df0f0f3c2d9fde32e8fc7a7bef4
Tomás González <tomasagustin.gonzalezorlando@arm.com>
2024-02-26 11:47:13 +0000

    Enable logging when provider is initialized
    Currently, errors are not displayed when they ocurr as there is no
    actual logger that has been included.
    
     * Add env_logger as the logging unit.
     * Initialize the logging unit when the provider is initialized.
    
    Signed-off-by: Tomás González <tomasagustin.gonzalezorlando@arm.com>

--- parsec-openssl-provider/src/lib.rs
+++ parsec-openssl-provider/src/lib.rs
@@ -27,6 +27,9 @@ pub unsafe fn parsec_provider_provider_init(
     out: *mut *const OSSL_DISPATCH,
     provctx: types::VOID_PTR_PTR,
 ) -> Result<(), parsec_openssl2::Error> {
+
+    env_logger::init();
+
     let parsec_provider_gettable_params_ptr: ProviderGettableParamsPtr =
         parsec_provider_gettable_params;
 
