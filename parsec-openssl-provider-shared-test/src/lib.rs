a3f9b89c27acc11e78caff989780944940cd53e1
Gowtham Suresh Kumar <gowtham.sureshkumar@arm.com>
2024-03-19 19:07:08 +0000

    test: Add support functions for loading a provider and key
    Signed-off-by: Gowtham Suresh Kumar <gowtham.sureshkumar@arm.com>

--- parsec-openssl-provider-shared-test/src/lib.rs
+++ parsec-openssl-provider-shared-test/src/lib.rs
@@ -12,3 +12,42 @@ pub mod openssl_test_bindings {
     include!(concat!(env!("OUT_DIR"), "/openssl_test_bindings.rs"));
 }
 
+// Needed to access as_ptr function for LibCtx
+pub use foreign_types_shared::ForeignType;
+pub use openssl_test_bindings::*;
+pub use parsec_openssl_provider::parsec_openssl2::openssl::{lib_ctx::LibCtx, provider::Provider};
+
+// These needs to be replaced with consts from the key management module
+pub const PARSEC_PROVIDER_RSA: &[u8; 4] = b"RSA\0";
+pub const PARSEC_PROVIDER_PROPERTY: &[u8; 17] = b"provider=default\0";
+
+// Loads a provider into the given library context
+pub fn load_provider(lib_ctx: &LibCtx, provider_name: &str, provider_path: String) -> Provider {
+    assert!(Provider::set_default_search_path(Some(lib_ctx), &provider_path).is_ok());
+    Provider::load(Some(lib_ctx), provider_name).unwrap()
+}
+
+// Loads a key using the given library context with loaded provider. The param should contain the necessary
+// parameters based on the provider that we are loading.
+pub unsafe fn load_key(lib_ctx: &LibCtx, param: *mut OSSL_PARAM, parsec_pkey: *mut *mut EVP_PKEY) {
+    let evp_ctx: *mut EVP_PKEY_CTX = EVP_PKEY_CTX_new_from_name(
+        lib_ctx.as_ptr() as *mut ossl_lib_ctx_st,
+        PARSEC_PROVIDER_RSA.as_ptr() as *const ::std::os::raw::c_char,
+        PARSEC_PROVIDER_PROPERTY.as_ptr() as *const ::std::os::raw::c_char,
+    );
+    assert_ne!(evp_ctx, std::ptr::null_mut());
+    assert_eq!(EVP_PKEY_fromdata_init(evp_ctx), 1);
+    assert_eq!(
+        EVP_PKEY_fromdata(
+            evp_ctx,
+            parsec_pkey as _,
+            // Change 3: Select the appropriate macro here to load the param value
+            EVP_PKEY_KEY_PARAMETERS.try_into().unwrap(),
+            param,
+        ),
+        1
+    );
+    assert_ne!(*parsec_pkey, std::ptr::null_mut());
+
+    EVP_PKEY_CTX_free(evp_ctx);
+}
